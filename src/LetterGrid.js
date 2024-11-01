d20e12b18632a830d95dba36c3a2d88f9e9b0144
bluedawn123 <haemilyjh@naver.com>
2024-11-01 16:41:41 +0900

    정답조건수정

--- src/LetterGrid.js
+++ src/LetterGrid.js
@@ -11,7 +11,7 @@ const LetterGrid = ({secretWord, guessedLetters, answerLength, complete})=>{
 
   //answer값이 변경되면 answerLength와 비교해서 정답여부 파악
   useEffect(()=>{
-    if(answer ===answerLength ){
+    if(answerLength > 0 && answer === answerLength ){
       alert('정답입니다!');
       complete();
     }
